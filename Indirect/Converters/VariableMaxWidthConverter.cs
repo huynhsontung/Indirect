d0030dde3cb2bd425065f8eae697f60c6ad0b457
Tung Huynh <tung75605@gmail.com>
2020-01-23 00:39:05 -0800

    add variable max width converter

---
+++ Indirect/Converters/VariableMaxWidthConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Indirect.Converters
+{
+    class VariableMaxWidthConverter : IValueConverter
+    {
+        public double MaxLengthSingle { get; set; } = 300;
+        public double MaxLengthMultiple { get; set; } = 220;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var length = (int) value;
+            return length > 42 ? MaxLengthMultiple : MaxLengthSingle;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
