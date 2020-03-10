8665dfc1d12020c37d2ed5978439d0732eaf9f81
Tung Huynh <tung75605@gmail.com>
2020-03-09 23:00:34 -0700

    fix visual bug

---
+++ Indirect/Converters/ReelShareImageMarginConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Indirect.Converters
+{
+    public class ReelShareImageMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var s = (string)value;
+            return s != "reply" ? new Thickness(0,0,0,0) : new Thickness(0, 0, 0, -12);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
