23058da1b30fca794873b74935bb66d02a614bc7
Tung Huynh <tung75605@gmail.com>
2020-08-11 01:22:01 -0700

    rapid toast implementation

--- BackgroundPushClient/Utils.cs
+++ BackgroundPushClient/Utils.cs
@@ -8,11 +8,16 @@ using InstagramAPI.Push;
 using Microsoft.Toolkit.Uwp.Notifications;
 using System;
 using System.IO;
+using System.Linq;
+using System.Threading;
 
 namespace BackgroundPushClient
 {
     internal sealed class Utils
     {
+        private static readonly ApplicationDataContainer LocalSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+        private static CancellationTokenSource _rapidToast = new CancellationTokenSource();
+
         public static void OnMessageReceived(object sender, PushReceivedEventArgs args)
         {
             var notificationContent = args.NotificationContent;
@@ -70,15 +75,44 @@ namespace BackgroundPushClient
             {
                 toast.RemoteId = notificationContent.PushId;
             }
-            // And send the notification
-            ToastNotificationManager.CreateToastNotifier().Show(toast);
+
+            QueueRapidToast(toast);
+        }
+
+        /// <summary>
+        /// Hide previous toast early if another toast is ready to show up
+        /// </summary>
+        /// <param name="toast"></param>
+        private static async void QueueRapidToast(ToastNotification toast)
+        {
+            _rapidToast?.Cancel();
+            _rapidToast = new CancellationTokenSource();
+            var notifier = ToastNotificationManager.CreateToastNotifier();
+            notifier.Show(toast);
+            try
+            {
+                await Task.Delay(5000, _rapidToast.Token);  // 5 seconds is default toast duration
+            }
+            catch (TaskCanceledException)
+            {
+                notifier.Hide(toast);   // Hide here will also remove toast from Action Center. We don't want that.
+                // Create a replicate toast and send it straight to AC.
+                // Cannot reuse the toast above because Show() doesn't allow a toast instance to be shown twice.
+                var replicateToast = new ToastNotification(toast.Content)
+                {
+                    Group = toast.Group,
+                    Tag = toast.Tag,
+                    ExpiresOnReboot = toast.ExpiresOnReboot,
+                    SuppressPopup = true
+                };
+                notifier.Show(replicateToast);
+            }
         }
 
         private static string GetThreadTitleFromAppSettings(string threadId)
         {
             if (string.IsNullOrEmpty(threadId)) return null;
-            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
-            var composite = (Windows.Storage.ApplicationDataCompositeValue)localSettings.Values[Instagram.THREAD_TITLE_PERSISTENT_DICTIONARY_KEY];
+            var composite = (Windows.Storage.ApplicationDataCompositeValue)LocalSettings.Values[Instagram.THREAD_TITLE_PERSISTENT_DICTIONARY_KEY];
             return (string) composite?[threadId];
         }
 
