4e7a96a53234a5ad28579a2cc3afa49b60ba560c
Tung Huynh <tung75605@gmail.com>
2021-06-09 16:52:55 -0700

    fix reply background task not running properly

--- BackgroundPushClient/ReplyAction.cs
+++ BackgroundPushClient/ReplyAction.cs
@@ -12,17 +12,17 @@ namespace BackgroundPushClient
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
             Instagram.StartAppCenter();
-            var session = await SessionManager.TryLoadLastSessionAsync();
-            if (session == null)
-            {
-                Utils.PopMessageToast("Reply failed. Account is not logged in. Tap this message to resolve this issue.");
-                return;
-            }
-
-            var instagram = new Instagram(session);
             var deferral = taskInstance.GetDeferral();
             try
             {
+                var session = await SessionManager.TryLoadLastSessionAsync();
+                if (session == null)
+                {
+                    Utils.PopMessageToast("Reply failed. Account is not logged in. Tap this message to resolve this issue.");
+                    return;
+                }
+
+                var instagram = new Instagram(session);
                 if (!instagram.IsUserAuthenticated) return;
                 if (taskInstance.TriggerDetails is ToastNotificationActionTriggerDetail details)
                 {
