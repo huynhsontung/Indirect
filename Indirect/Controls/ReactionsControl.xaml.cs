7c2d8dd8f42af1b919bde8bf42342f53e157fb0e
Tung Huynh <tung75605@gmail.com>
2021-07-02 11:49:13 -0700

    update to winui 2.6

--- Indirect/Controls/ReactionsControl.xaml.cs
+++ Indirect/Controls/ReactionsControl.xaml.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Indirect.Entities.Wrappers;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -36,7 +27,7 @@ namespace Indirect.Controls
             get => (ObservableCollection<ReactionWithUser>) GetValue(ReactionsProperty);
             set => SetValue(ReactionsProperty, value);
         }
-        
+
         private string FirstEmoji { get; set; }
         
         private string SecondEmoji { get; set; }
