87e6af297a73c26ea2446c1971e47148ab513718
Tung Huynh <tung75605@gmail.com>
2022-05-30 12:29:22 -0700

    clean up MainPage

---
+++ Indirect/Controls/NewMessagePicker.xaml.cs
@@ -0,0 +1,141 @@
+﻿using InstagramAPI.Classes.User;
+using Microsoft.Toolkit.Uwp.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Indirect.Controls
+{
+    public sealed partial class NewMessagePicker : UserControl
+    {
+        private MainViewModel ViewModel => ((App)Application.Current).ViewModel;
+
+        private ObservableCollection<BaseUser> NewMessageCandidates { get; } = new ObservableCollection<BaseUser>();
+        private Flyout _flyout;
+
+        public NewMessagePicker()
+        {
+            this.InitializeComponent();
+        }
+
+        public static Flyout GetFlyout()
+        {
+            Flyout flyout = new()
+            {
+                Placement = FlyoutPlacementMode.RightEdgeAlignedTop
+            };
+
+            NewMessagePicker control = new();
+            control._flyout = flyout;
+            flyout.Content = control;
+            return flyout;
+        }
+
+        private void NewMessageSuggestBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason != AutoSuggestionBoxTextChangeReason.UserInput) return;
+            if (string.IsNullOrEmpty(sender.Text) || sender.Text.Length > 50)
+            {
+                return;
+            }
+
+            ViewModel.SearchWithoutThreads(sender.Text,
+                updatedList => NewMessageSuggestBox.ItemsSource = updatedList);
+        }
+
+        private void NewMessageSuggestBox_OnSuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            var selectedItem = (BaseUser)args.SelectedItem;
+            sender.Text = selectedItem.Username;
+        }
+
+        private void NewMessageSuggestBox_OnQuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if (args.ChosenSuggestion != null)
+            {
+                var selectedRecipient = (BaseUser)args.ChosenSuggestion;
+                if (NewMessageCandidates.All(x => selectedRecipient.Username != x.Username))
+                    NewMessageCandidates.Add(selectedRecipient);
+            }
+            else if (!string.IsNullOrEmpty(sender.Text))
+            {
+                ViewModel.SearchWithoutThreads(sender.Text, updatedList =>
+                {
+                    if (updatedList.Count == 0) return;
+                    NewMessageCandidates.Add(updatedList[0]);
+                });
+            }
+            sender.Text = string.Empty;
+            sender.ItemsSource = null;
+        }
+
+        private void NewMessageClearAll_OnClick(object sender, RoutedEventArgs e)
+        {
+            NewMessageCandidates.Clear();
+        }
+
+        private async void ChatButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _flyout.Hide();
+            if (NewMessageCandidates.Count == 0 || NewMessageCandidates.Count > 32) return;
+            var userIds = NewMessageCandidates.Select(x => x.Pk);
+            await ViewModel.CreateAndOpenThread(userIds);
+            NewMessageCandidates.Clear();
+        }
+
+        private void ClearSingleCandidateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var target = (BaseUser)(sender as FrameworkElement)?.DataContext;
+            if (target == null) return;
+            NewMessageCandidates.Remove(target);
+        }
+
+        private void Candidate_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (((FrameworkElement)sender).FindDescendant("ClearSingleCandidateButton") is Button button)
+            {
+                button.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Candidate_OnPointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            if (((FrameworkElement)sender).FindDescendant("ClearSingleCandidateButton") is Button button)
+            {
+                button.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void ClearSingleCandidateSwipe_OnInvoked(SwipeItem sender, SwipeItemInvokedEventArgs args)
+        {
+            var target = (BaseUser)args.SwipeControl.DataContext;
+            if (target == null) return;
+            NewMessageCandidates.Remove(target);
+        }
+
+        private void NewMessageSuggestBox_OnProcessKeyboardAccelerators(UIElement sender, ProcessKeyboardAcceleratorEventArgs args)
+        {
+            if (args.Key == VirtualKey.Escape && args.Modifiers == VirtualKeyModifiers.None)
+            {
+                args.Handled = true;
+                _flyout.Hide();
+            }
+
+        }
+    }
+}
--- Indirect/Pages/MainPage.xaml.cs
+++ Indirect/Pages/MainPage.xaml.cs
@@ -47,7 +47,6 @@ namespace Indirect.Pages
         }
 
         private MainViewModel ViewModel => ((App) Application.Current).ViewModel;
-        private ObservableCollection<BaseUser> NewMessageCandidates { get; } = new ObservableCollection<BaseUser>();
         private CoreViewHandle _reelPageView;
         private bool _loggedOut;
 
@@ -327,102 +326,6 @@ namespace Indirect.Pages
 
             sender.Text = string.Empty;
             sender.ItemsSource = null;
-        }
-
-        #endregion
-
-        #region NewMessage
-
-        private void NewMessageSuggestBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
-        {
-            if (args.Reason != AutoSuggestionBoxTextChangeReason.UserInput) return;
-            if (string.IsNullOrEmpty(sender.Text) || sender.Text.Length > 50)
-            {
-                return;
-            }
-
-            ViewModel.SearchWithoutThreads(sender.Text,
-                updatedList => NewMessageSuggestBox.ItemsSource = updatedList);
-        }
-
-        private void NewMessageSuggestBox_OnSuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
-        {
-            var selectedItem = (BaseUser) args.SelectedItem;
-            sender.Text = selectedItem.Username;
-        }
-
-        private void NewMessageSuggestBox_OnQuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
-        {
-            if (args.ChosenSuggestion != null)
-            {
-                var selectedRecipient = (BaseUser) args.ChosenSuggestion;
-                if (NewMessageCandidates.All(x => selectedRecipient.Username != x.Username))
-                    NewMessageCandidates.Add(selectedRecipient);
-            }
-            else if (!string.IsNullOrEmpty(sender.Text))
-            {
-                ViewModel.SearchWithoutThreads(sender.Text, updatedList =>
-                {
-                    if (updatedList.Count == 0) return;
-                    NewMessageCandidates.Add(updatedList[0]);
-                });
-            }
-            sender.Text = string.Empty;
-            sender.ItemsSource = null;
-        }
-
-        private void NewMessageClearAll_OnClick(object sender, RoutedEventArgs e)
-        {
-            NewMessageCandidates.Clear();
-        }
-
-        private async void ChatButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            NewThreadFlyout.Hide();
-            if (NewMessageCandidates.Count == 0 || NewMessageCandidates.Count > 32) return;
-            var userIds = NewMessageCandidates.Select(x => x.Pk);
-            await ViewModel.CreateAndOpenThread(userIds);
-            NewMessageCandidates.Clear();
-        }
-
-        private void ClearSingleCandidateButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            var target = (BaseUser) (sender as FrameworkElement)?.DataContext;
-            if (target == null) return;
-            NewMessageCandidates.Remove(target);
-        }
-
-        private void Candidate_PointerEntered(object sender, PointerRoutedEventArgs e)
-        {
-            if (((FrameworkElement)sender).FindDescendant("ClearSingleCandidateButton") is Button button)
-            {
-                button.Visibility = Visibility.Visible;
-            }
-        }
-
-        private void Candidate_OnPointerExited(object sender, PointerRoutedEventArgs e)
-        {
-            if (((FrameworkElement)sender).FindDescendant("ClearSingleCandidateButton") is Button button)
-            {
-                button.Visibility = Visibility.Collapsed;
-            }
-        }
-
-        private void ClearSingleCandidateSwipe_OnInvoked(SwipeItem sender, SwipeItemInvokedEventArgs args)
-        {
-            var target = (BaseUser) args.SwipeControl.DataContext;
-            if (target == null) return;
-            NewMessageCandidates.Remove(target);
-        }
-
-        private void NewMessageSuggestBox_OnProcessKeyboardAccelerators(UIElement sender, ProcessKeyboardAcceleratorEventArgs args)
-        {
-            if (args.Key == VirtualKey.Escape && args.Modifiers == VirtualKeyModifiers.None)
-            {
-                args.Handled = true;
-                NewThreadFlyout.Hide();
-            }
-                
         }
 
         #endregion
