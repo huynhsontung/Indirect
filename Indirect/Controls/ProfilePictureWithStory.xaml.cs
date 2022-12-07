1cb4b3e05c3a07518a8997726da02e81f02655df
Tung Huynh <tung75605@gmail.com>
2022-12-07 00:32:25 -0800

    feat: open reel from thread view

--- Indirect/Controls/ProfilePictureWithStory.xaml.cs
+++ Indirect/Controls/ProfilePictureWithStory.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using Indirect.Entities;
 using InstagramAPI.Classes.User;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -44,7 +33,6 @@ namespace Indirect.Controls
         public ProfilePictureWithStory()
         {
             DataContext = ViewModel = new ProfilePictureWithStoryViewModel();
-            ViewModel.PropertyChanged += ViewModel_PropertyChanged;
             this.InitializeComponent();
         }
 
@@ -52,31 +40,6 @@ namespace Indirect.Controls
 
         private Brush GetIndicatorStrokeBrush(bool unseen) => unseen ? (Brush)Resources["StoryBrush"] : (Brush)App.Current.Resources["ControlElevationBorderBrush"];
 
-        private void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(ViewModel.HasReel))
-            {
-                UpdatePictureSize();
-            }
-        }
-
-        private void ProfilePictureWithStory_OnSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            UpdatePictureSize();
-        }
-
-        private void UpdatePictureSize()
-        {
-            if (ViewModel.HasReel)
-            {
-                Picture.Width = ActualWidth - 8;
-                Picture.Height = ActualHeight - 8;
-            }
-            else
-            {
-                Picture.Width = double.NaN;
-                Picture.Height = double.NaN;
-            }
-        }
+        private Thickness GetPictureMargin(bool hasReel) => hasReel ? new Thickness(4) : default;
     }
 }
